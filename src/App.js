f8e447484e88b6deabce08b95ba65b2eac379f97
trbogi <trencsenyi.bogi@gmail.com>
2022-11-07 22:15:14 +0100

    Style output field

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
       <div className="h-screen">
-          <div style={{backgroundImage: `url(${background})`}} className="h-1/3 flex justify-center">
+          <div style={{backgroundImage: `url(${background})`}} className="h-1/3 flex justify-center relative">
               <div className="flex flex-col w-[500px]">
                   <h1 className="text-center mt-4 mb-4">IP Address Tracker</h1>
                   <div className="w-full flex flex-start">
@@ -13,9 +13,28 @@ function App() {
                       <button type="button" className="bg-slate-900 w-1/12 rounded-r-lg flex justify-center items-center"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6"/></svg></button>
                   </div>
               </div>
+              <div className="h-[128px] w-4/5 bg-slate-50 absolute z-[1000] -bottom-16 rounded-lg py-6 grid grid-cols-4 divide-x-[2px]">
+                  <div className="px-6">
+                      <p className="uppercase text-slate-500 text-xs font-bold mb-2">Ip address</p>
+                      <p className="font-semibold text-2xl">tEXt</p>
+                  </div>
+                  <div className="px-6">
+                      <p className="uppercase text-slate-500 text-xs font-bold mb-2">Location</p>
+                      <p className="font-semibold text-2xl">tEXt</p>
+                  </div>
+                  <div className="px-6">
+                      <p className="uppercase text-slate-500 text-xs font-bold mb-2">Timezone</p>
+                      <p className="font-semibold text-2xl">tEXt</p>
+                  </div>
+                  <div className="px-6">
+                      <p className="uppercase text-slate-500 text-xs font-bold mb-2">Isp</p>
+                      <p className="font-semibold text-2xl">tEXt</p>
+                  </div>
+              </div>
           </div>
 
-          <MapContainer className="h-2/3" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+
+          <MapContainer className="h-2/3 z-0" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
               <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

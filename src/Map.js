738ec6b13a2bb5bc1e2a47ec2d7bd5ffb8334176
trbogi <trencsenyi.bogi@gmail.com>
2022-11-08 13:45:22 +0100

    Create Map component

---
+++ src/Map.js
@@ -0,0 +1,26 @@
+import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
+
+function Map({latitude, longitude}) {
+    const center = [latitude, longitude]
+    const zoom = 13
+
+    function ChangeView({ center, zoom }) {
+        const map = useMap();
+        map.setView(center, zoom);
+        return null;
+    }
+    return(
+        <MapContainer className="absolute w-full top-[254px] md:top-[200px] bottom-0 z-0" center={center} zoom={zoom} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <ChangeView center={center} zoom={zoom} />
+            <Marker position={[latitude, longitude]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>)
+
+}export default Map
